eb7f8884fea3a76da56cbe256a6b981a0d3c0e8a
jcbusche <jcbusche@gmail.com>
2013-12-08 18:54:31 -0500

    First formatting update

--- views/_v_template.php
+++ views/_v_template.php
@@ -14,23 +14,46 @@
 
 	<div id='menu'>
 
-        <a href='/'>Home</a>
+
+        <a href='/'><span id = "logo"><?php echo strtoupper(APP_NAME);?></a>
+        <br>
 
         <!-- Menu for users who are logged in -->
         <?php if($user): ?>
+            <ul>
+                <li>
+                    <a href='/users/profile'>Profile</a>
+                </li>    
+                    
+                <li>
+                    <a href='/posts'>Your Feed</a>
+                </li>
 
-            
-            <a href='/users/profile'>Profile</a>
-            <a href='/posts'>Your Feed</a>
-            <a href='/posts/add'>New post</a>
-            <a href='/posts/users'>Manage Subscriptions</a>
-            <a href='/users/logout'>Logout</a>
+                <li>
+                    <a href='/posts/add'>New post</a>
+                </li>
+
+                <li>
+                    <a href='/posts/users'>Manage Subscriptions</a>
+                </li> 
+                
+                <li>
+                    <a href='/users/logout'>Logout</a>
+                </li>
+            </ul>
 
         <!-- Menu options for users who are not logged in -->
         <?php else: ?>
 
-            <a href='/users/signup'>Sign up</a>
-            <a href='/users/login'>Log in</a>
+            <ul>
+                <li>
+                    <a href='/users/signup'>Sign up</a>
+                </li>
+
+                <li>    
+                    <a href='/users/login'>Log in</a>
+                </li>
+            </ul>    
 
         <?php endif; ?>
 
