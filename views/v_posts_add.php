dd4f30a151becd8ff1c264bb9008d36626c5c6d2
jcbusche <jcbusche@gmail.com>
2013-12-08 21:00:56 -0500

    formatting

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,4 +1,16 @@
+<?php if(isset($error)):?>
+	<div class='error' align "center">
+
+		Post is empty.
+		<br>
+	</div>
+<?php endif;?>
+
+<?php else:?>
+
 <form method='POST' action='/posts/p_add'>
+
+<?php endif;?>
 
     <label for='content'>New Post:</label><br>
     <textarea name='content' id='content'></textarea>
