027b36032679e27f27568e810dec197aaed9f2fc
jcbusche <jcbusche@gmail.com>
2013-12-08 22:08:12 -0500

    formatting hell

--- views/_v_template.php
+++ views/_v_template.php
@@ -3,7 +3,10 @@
 <head>
 	<title><?php if(isset($title)) echo $title; ?></title>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="/css/main.css" type="text/css">	
 
     
 	
