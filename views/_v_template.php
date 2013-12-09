6c0394b40f21e064c687166b8ea33a916933e794
jcbusche <jcbusche@gmail.com>
2013-12-08 21:28:21 -0500

    formatting

--- views/_v_template.php
+++ views/_v_template.php
@@ -5,12 +5,7 @@
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
 
-    <!--CSS-->
-    <!--<link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
-    <link rel="stylesheet" href="/css/main.css" type="text/css">-->
-					
-	<!-- Controller Specific JS/CSS -->
-	<!--<?php if(isset($client_files_head)) echo $client_files_head; ?>-->
+    
 	
 </head>
 
